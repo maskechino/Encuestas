cfddf0af86690d3ae1fde6baf53ffa8053d8da66
Jorge <maskechino@gmail.com>
2020-06-05 19:43:14 -0400

    #3

--- encuestas2.py
+++ encuestas2.py
@@ -10,7 +10,29 @@ class Encuesta:
     def imprimirtitulo(self):
         print(self.titulo)
 
+class PreguntaYrespuestas:
+
+    def __init__(self):
+        pass
+
+    def preguntaYrespuestas(self, pregunta, listadoRespuestas):
+        self.pregunta = pregunta
+        self.listadoRespuestas = listadoRespuestas
+
+    def imprimirtitulo(self):
+        print(self.listadoRespuestas)
+
 encuesta = Encuesta()
+
+preguntaYrespuestas = PreguntaYrespuestas()
+
+listadoRespuestas = []
+
+PreguntaYrespuestas.listadoRespuestas = listadoRespuestas
+
+preguntaYrespuestas.pregunta = ""
+
+
 
 encuesta.titulo = input("Ingresar titulo:\n")
 
@@ -18,16 +40,25 @@ ingresarPregunta = input("Desesa ingresar una pregunta?:")
 
 while ingresarPregunta == "si":
     pregunta = input("Ingrese pregunta:\n")
+    preguntaYrespuestas.pregunta = pregunta
     continuar = "si"
-    indice = 0
-
-    while indice < 4 and continuar == "si":
+    indice = 1
+    
+    while indice < 4 and continuar == "si" :                
         respuesta = input("Ingrese respuesta:\n")
+        listadoRespuestas.append(respuesta)
         continuar = input("Desea ingresar otra respuesta:\n")
         indice = indice + 1
-    ingresarPregunta = input("Desea agregar otra pregunta?\n")
+        
+    #continuar = input("Desea ingresar otra respuesta:\n")
+    ingresarPregunta = input("Desesa ingresar una pregunta?:")
+ 
+listadoRespuestas.append(respuesta)
+preguntaYrespuestas.pregunta = pregunta
+    
 
 encuesta.imprimirtitulo()
+preguntaYrespuestas.imprimirtitulo()
 
 
 
