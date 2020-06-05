d366bc0d8bbb1a6a1bdfa77fba736be483b297fd
Jorge <maskechino@gmail.com>
2020-06-05 02:26:02 -0400

    ingreso de pregunta con hasta 4 respuestas

---
+++ encuestas2.py
@@ -0,0 +1,47 @@
+class Encuesta:
+  def __init__(self, titulo, listadepreguntas):
+    self.titulo = titulo #titulo de la encuesta#
+    self.listadepreguntas = listadepreguntas #lista de preguntas con sus respectivas respuestas#
+    
+class preguntayrespuestas:
+  def __init__(self, pregunta, respuestas):
+      self.pregunta = pregunta
+      self.respuestas = respuestas
+
+titulo = input("ingresa el titulo:\n")
+listado = []
+pregunta = input("Ingresa tu pregunta:")
+respuestas = []
+contador = 0
+while contador < 4:
+    respuesta = input("ingresa tu respuesta:")
+    respuestas.append(respuesta)
+    contador = contador + 1
+
+else:
+    print("Alcanzaste el maximo de respuestas")
+    listado.append(pregunta)
+    listado.append(respuestas)
+    continuar = "si"
+    while input("Desea continuar con otra pregunta?:") == continuar:
+        contador = 0
+        pregunta = input("ingresa otra pregunta:")
+        respuestas = []
+        while contador < 4:
+            respuesta = input("ingresa tu respuesta:")
+            respuestas.append(respuesta)
+            contador = contador + 1
+        else:
+            listado.append(pregunta)
+            listado.append(respuestas)
+
+
+
+encuestanueva = Encuesta(titulo, listado)
+
+
+print(titulo)
+print(listado)
+
+
+#titulo = input("Ingresa el titulo:") #ingreso del titulo#
