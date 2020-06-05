a192e785673a6d2385f07b455f91c2e66151ec3d
Jorge <maskechino@gmail.com>
2020-06-05 05:30:29 -0400

    Ingreso QA con For
    prueba con funcion for

--- encuestas2.py
+++ encuestas2.py
@@ -1,47 +1,61 @@
 class Encuesta:
-  def __init__(self, titulo, listadepreguntas):
-    self.titulo = titulo #titulo de la encuesta#
-    self.listadepreguntas = listadepreguntas #lista de preguntas con sus respectivas respuestas#
+
+    def __init__(self, titulo, listadepreguntas):
+        self.titulo = titulo #titulo de la encuesta#
+        self.listado = listado #lista de preguntas con sus respectivas respuestas#
+
+    def imprimirencuesta(self):
+        print(self.listado)
     
-class preguntayrespuestas:
-  def __init__(self, pregunta, respuestas):
+class Preguntayrespuestas:
+    def __init__(self, pregunta, respuestas):
       self.pregunta = pregunta
       self.respuestas = respuestas
 
+class Listado:
+    def  __init__ (self, listado):
+        self.listado = listado
+
 titulo = input("ingresa el titulo:\n")
-listado = []
+
+
 pregunta = input("Ingresa tu pregunta:")
 respuestas = []
+listado = []
 contador = 0
-while contador < 4:
-    respuesta = input("ingresa tu respuesta:")
+
+preguntayrespuestas = Preguntayrespuestas(pregunta, respuestas)
+
+
+for i in range(1, 5):
+    print("ingresa respuesta")
+    respuesta = input()
     respuestas.append(respuesta)
     contador = contador + 1
 
 else:
     print("Alcanzaste el maximo de respuestas")
-    listado.append(pregunta)
-    listado.append(respuestas)
+    listado.append(preguntayrespuestas)
     continuar = "si"
     while input("Desea continuar con otra pregunta?:") == continuar:
         contador = 0
         pregunta = input("ingresa otra pregunta:")
         respuestas = []
-        while contador < 4:
-            respuesta = input("ingresa tu respuesta:")
+        for i in range(1, 5):
+            print("ingresa respuesta")
+            respuesta = input()
             respuestas.append(respuesta)
             contador = contador + 1
         else:
-            listado.append(pregunta)
-            listado.append(respuestas)
+            listado.append(preguntayrespuestas)
+
+listado = Listado(listado)
+
 
 
 
 encuestanueva = Encuesta(titulo, listado)
 
 
-print(titulo)
+
 print(listado)
-
-
-#titulo = input("Ingresa el titulo:") #ingreso del titulo#
